aabd78b411d358e0c84054004c69a0e2571e13bb
David Peden <davidpeden3@gmail.com>
2019-01-30 22:01:06 -0600

    initial commit

---
+++ Source/PvPoke/GameMasterFileManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PvPoke
+{
+    public static class GameMasterFileManager
+    {
+        private static readonly string _gameMasterVersionUri = "https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest-version.txt";
+        private static readonly string _gameMasterJsonUri = "https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json";
+        private static readonly HttpClient _client = new HttpClient();
+
+        private static string GameMasterJsonPath => Path.Combine(AppContext.BaseDirectory, "Data", "gameMaster.json");
+
+        public static bool FileExists()
+        {
+            return File.Exists(GameMasterJsonPath);
+        }
+
+        public static async Task<long> FetchLatestVersionAsync()
+        {
+            Console.WriteLine("Fetching latest version #...");
+            var currentGameMasterVersion = Convert.ToInt64(await _client.GetStringAsync(_gameMasterVersionUri));
+            return currentGameMasterVersion;
+        }
+
+        public static async Task<GameMasterFile> OpenFileAsync()
+        {
+            Console.WriteLine("Reading file...");
+            string json = await File.ReadAllTextAsync(GameMasterJsonPath);
+
+            return JsonConvert.DeserializeObject<GameMasterFile>(json);
+        }
+
+        public static async Task<GameMasterFile> FetchAndSaveFileAsync()
+        {
+            Console.WriteLine("Fetching file...");
+            string json = await _client.GetStringAsync(_gameMasterJsonUri);
+
+            Console.WriteLine("Writing file...");
+            await File.WriteAllTextAsync(GameMasterJsonPath, json);
+
+            return JsonConvert.DeserializeObject<GameMasterFile>(json);
+        }
+    }
+}
---
+++ Source/PvPoke/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PvPoke
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            GameMasterFile gameMasterFile;
+
+            if (!GameMasterFileManager.FileExists())
+            {
+                gameMasterFile = await GameMasterFileManager.FetchAndSaveFileAsync();
+            }
+            else
+            {
+                long latestGameMasterVersion = await GameMasterFileManager.FetchLatestVersionAsync();
+
+                gameMasterFile = await GameMasterFileManager.OpenFileAsync();
+
+                if (gameMasterFile.TimeStampMs != latestGameMasterVersion)
+                {
+                    gameMasterFile = await GameMasterFileManager.FetchAndSaveFileAsync();
+                }
+            }
+
+            Console.WriteLine(gameMasterFile.TimeStampMs);
+        }
+    }
+
+    public class GameMasterFile
+    {
+        public long TimeStampMs { get; set; }
+    }
+}
